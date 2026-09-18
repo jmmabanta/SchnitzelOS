2b07bb49271a20159ca57444622b1932b62fcdd7
John M. <johnmabanta51@gmail.com>
2026-09-18 09:00:48 -0600

    refactor(build): directly specify dracut drivers in command
    This gets rid of the unnecessary dracut/dracut.conf.d folder.
    Also got rid of the fido2 module as I don't need it.

--- build_files/70-build-initramfs.sh
+++ build_files/70-build-initramfs.sh
@@ -11,6 +11,6 @@ else
 fi
 
 QUALIFIED_KERNEL="$(dnf5 repoquery --installed --queryformat='%{evr}.%{arch}' "kernel${KERNEL_SUFFIX:+-${KERNEL_SUFFIX}}")"
-/usr/bin/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible --zstd -v --add ostree --add fido2 -f "/usr/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
+/usr/bin/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible --zstd -v --add ostree --add-drivers "lz4hc lz4hc_compress" -f "/usr/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
 
 chmod 0600 /usr/lib/modules/"$QUALIFIED_KERNEL"/initramfs.img
